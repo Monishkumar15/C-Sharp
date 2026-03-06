8a9a11d48682fb2abfae9fe830fa6c36b52f3ab5
Monishkumar15 <monishkumarr567@gmail.com>
2026-03-06 19:57:34 +0530

    feat: add the Interface using multiple Inheritance and Enums

---
+++ oops/Interface/Days.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Interface
+{
+    public enum Days
+    {
+        Monday = 1,
+        Tuesday = 11,
+        Wednesday = 22,
+        Thursday = 33,
+        Friday = 44,
+
+    }
+    public class TestClass
+    {
+
+        public static Days MeetingDate
+        {
+            get; set;
+        } = (Days)1;
+        //public static Days MeetingDate
+        //{
+        //    get; set;
+        //} = 1; Cannot implicitly convert type 'int' to 'Interface.Days'. An explicit conversion exists (are you missing a cast?)
+        static void Main()
+        {
+            foreach(int i in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach(string i in Enum.GetNames(typeof(Days)))
+            {
+                Console.WriteLine(i);
+            }
+            foreach(int i in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine(i+" : "+(Days)i);
+            }
+
+            Console.WriteLine(MeetingDate);
+            MeetingDate = Days.Wednesday;
+            Console.WriteLine(MeetingDate);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ oops/Interface/Interface1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface Interface1
+    {
+        void Test();
+        void Show();
+    }
+
+    public interface Interface2
+    {
+        void Test();
+        void Show();
+    }
+
+    class MultipleInheritanceClass : Interface1, Interface2
+    {
+
+        //Normal Implementation
+        public void Show()
+        {
+            Console.WriteLine("Interfaces method show method implemented in child class");
+        }
+
+
+        //Explicity Implementation
+        void Interface1.Test()
+        {
+            Console.WriteLine("Declared in Interface1 and implemented in child class");
+        }
+
+        void Interface2.Test()
+        {
+            Console.WriteLine("Declared in Interface2 and implemented in child class");
+        }
+
+        public static void Main()
+        {
+            MultipleInheritanceClass m = new MultipleInheritanceClass();
+            m.Show();
+            //m.Test(); 'MultipleInheritanceClass' does not contain a definition for 'Test' and no accessible extension method 'Test' accepting a first argument of type 'MultipleInheritanceClass' could be found
+
+            Interface1 i1 = new MultipleInheritanceClass();
+            i1.Show();
+            i1.Test();
+
+            Interface2 i2 = new MultipleInheritanceClass();
+            i2.Show();
+            i2.Test();
+        }
+    }
+}
