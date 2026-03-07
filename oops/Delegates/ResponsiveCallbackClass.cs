7f38111381865c2f2d6921417dd257a78cb804d2
Monishkumar15 <monishkumarr567@gmail.com>
2026-03-07 19:17:56 +0530

    feat: add ResponsiveCallBack Delagates

---
+++ oops/Delegates/ResponsiveCallbackClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class ResponsiveCallbackClass
+    {
+        delegate void ResponseDelegate(string result);
+        static void ProcessData(ResponseDelegate response)
+        {
+            // simulate processing
+            string data = "Processing completed";
+            response(data); // send response back
+        }
+
+        static void ShowResult(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main()
+        {
+            ProcessData(ShowResult);
+        }
+    }
+}
